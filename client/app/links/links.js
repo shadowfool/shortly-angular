78aff51b71bb700567ddb1ce99cd93b408b3a281
shadowfool <aaronf101@gmail.com>
2016-05-20 21:12:06 -0700

    When commited adds filtered search of links

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,6 +2,6 @@ angular.module('shortly.links', [])
 .controller('LinksController', function($scope, Links) {
   $scope.data = {};
   Links.getAll().then(function(data) {
-    $scope.data.links = data;
+    return data;
   });
 });
